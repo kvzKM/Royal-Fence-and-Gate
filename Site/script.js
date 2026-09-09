7c229e694132e6d94d2a21324e52f6f2212b5988
Katie <iamkatieakaicy@gmail.com>
2026-09-09 11:02:49 +0200

    Add files via upload

---
+++ Site/script.js
@@ -0,0 +1,13 @@
+// ===============================
+// ROYAL GATE & FENCE
+// Main JavaScript
+// ===============================
+
+
+// Page loaded
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    console.log("Royal Gate & Fence website loaded.");
+
+});
